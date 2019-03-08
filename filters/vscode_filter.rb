c01d154bf052726dc2948c08ad7fa1dd78f3684d
Joey <513358386@qq.com>
2019-03-08 17:33:30 +0800

    Code command to fast open vscode by file path

--- filters/vscode_filter.rb
+++ filters/vscode_filter.rb
@@ -1,115 +1,141 @@
 require_relative '../utils/output'
 
 class VscodeFilter
-  ORG_GITHUB_HOST = 'https://github.com/Overseas-Student-Living/'
-
+  INVALID_PATH_CONTENT = { :title => "Can't find the file or directory with current path of" }
   class << self
     def do_filter(key)
-      # [{:uid=>"desktop", :type=>"file", :title=>"Desktop", :subtitle=>"~/Desktop", :arg=>"~/Music", :autocomplete=>"Desktop", :icon=>{:type=>"fileicon", :path=>"~/Music"}}]
-      # Dir.home # => "/Users/joeeey"
+      # Check `code` cli
+      if !File.exist? '/usr/local/bin/code'
+        Output.put({
+          :title => "Can't find the `code` cli for vscode.",
+          :subtitle => "press SHIFT+ENTER go to offical install doc.",
+          :arg => "https://code.visualstudio.com/docs/setup/mac",
+          :autocomplete => "https://code.visualstudio.com/docs/setup/mac"
+        })
+        return
+      end
 
       # automplete 带有 xxx/, do filter 中发现 / 结尾发触发 Dir.entries
       # 否则只做过滤
-
-
-      # current_children_files = Dir.entries(key)
-      # a = {}
-      # Dir.foreach(Dir.home) do |file|
-      #   a[file] = File.directory? file
-      #   # puts File.directory? file
-      # end
-
-      # p a
-
       if key && key != ''
-        if key[0] == '~'
-          # todo
-          current_path = "#{Dir.home}/#{key[1..-1]}"
-        elsif !key.include?(Dir.home)
-          current_path = "#{Dir.home}/#{key}"
-        else
-          current_path = key
+        if !key.downcase.include?(Dir.home.downcase) && 
+           !Dir.home.downcase.include?(key.downcase) &&
+           key[0] != '/'
+          key = "#{Dir.home}/#{key}"
         end
       else
-        current_path = "#{Dir.home}/"
-        # If key not exist, use '~' path defaultly  
+        # If key not exist, use `~` path defaultly
+        key = "#{Dir.home}/"
       end
-      if current_path[-1] == '/'
-        entry_path = current_path
-        current_children_files = Dir.entries(entry_path)
-        file_name_mapping = {}
-        current_children_files.each do |current_children_file|
-          file_name_mapping[current_children_file] = current_children_file
+      if key[-1] == '/'
+        entry_path = key
+
+        if !File.exist?(entry_path)
+          Output.put(get_invalid_path_msg(entry_path))
+          return
         end
-        items = get_items('', file_name_mapping, current_path)
+        file_name_mapping = get_file_mapping(entry_path)
+        items = get_items('', file_name_mapping, key)
 
         Output.put(items)
-      else 
-        slash_indexes = (0 ... current_path.length).find_all { |i| current_path[i,1] == '/' }
+      else
+        slash_indexes = (0 ... key.length).find_all { |i| key[i,1] == '/' }
 
         last_slash_index = slash_indexes.last
-        entry_path = current_path[0..last_slash_index]
-        current_children_files = Dir.entries(entry_path)
-
-        file_name_mapping = {}
-        current_children_files.each do |current_children_file|
-          file_name_mapping[current_children_file] = current_children_file
-        end
+        entry_path = key[0..last_slash_index]
 
         filter_key = key[(last_slash_index+1)..-1]
-        items = get_items(filter_key, file_name_mapping, current_path)
+
+        if !File.exist?(entry_path)
+          Output.put(get_invalid_path_msg(entry_path))
+          return
+        end
+        file_name_mapping = get_file_mapping(entry_path)
+
+        items = get_items(filter_key, file_name_mapping, entry_path)
         Output.put(items)
       end
-      return
+    end
+
+    def get_invalid_path_msg(path)
+      return { :title => "Can't find path of #{path}" }
+    end
+
+    def get_file_mapping(entry_path)
+      current_children_files = Dir.entries(entry_path).sort
+
+      # hidden_files is file or dir begin with `.`
+      hidden_files = current_children_files.select { |x| x[0]=='.' }
+      explicit_files = current_children_files - hidden_files
+      current_children_files = explicit_files + hidden_files
+
+      file_name_mapping = {}
+      current_children_files.each do |current_children_file|
+        file_name_mapping[current_children_file] = current_children_file
+      end
+      return file_name_mapping
     end
 
     # 获取生成 alfred 文档的元素
-    def get_items(filter_key, file_name_mapping, current_path)
-      # p "get_matched_keys is #{file_name_mapping}, #{filter_key}"
+    def get_items(filter_key, file_name_mapping, key)
       matchedKeys = get_matched_keys(file_name_mapping, filter_key)
       if matchedKeys.size == 0
-        return { :title => "404 NOT FOUND." }
+        return get_invalid_path_msg(key + filter_key)
       elsif matchedKeys.size == 1
         matchedKey = matchedKeys[0]
-        file_name = file_name_mapping[matchedKey]
-        file_path = current_path + file_name
-        return {
-          :type=>"file", 
-          :title=>file_name, 
-          :arg=>"#{File.directory?(current_path+file_name) ? (file_path + '/') : file_path}", 
-          :autocomplete=>"#{File.directory?(current_path+file_name) ? (file_path + '/') : file_path}", 
-          :icon=>{:type=>"fileicon", :path=>file_path}
-        }
+
+        item = get_hash_item(matchedKey, file_name_mapping, key)
+        return item
       else
         items = []
         matchedKeys.each do |matchedKey|
-          file_name = file_name_mapping[matchedKey]
-          file_path = current_path + file_name
-          items <<  {
-            :type=>"file",
-            :title=>file_name, 
-            :arg=>"#{File.directory?(current_path+file_name) ? (file_path + '/') : file_path}", 
-            :autocomplete=>"#{File.directory?(current_path+file_name) ? (file_path + '/') : file_path}", 
-            :icon=>{:type=>"fileicon", :path=>file_path}
-          }    
+          item = get_hash_item(matchedKey, file_name_mapping, key)
+          items << item
         end
         return items
       end
     end
 
     private
+    def get_hash_item(matchedKey, file_name_mapping, key)
+      file_name = file_name_mapping[matchedKey]
+      file_path = key + file_name
+      return {
+        # :type=>"file", // annotation this to disable shoft preview
+        :title=>file_name,
+        :arg=>"#{File.directory?(key+file_name) ? (file_path) : file_path}", 
+        :autocomplete=>"#{File.directory?(key+file_name) ? (file_path) : file_path}", 
+        :icon=>{:type=>"fileicon", :path=>file_path}
+      }
+    end
+
+    # Split the string and check left right side is matched.
+    # Make sure str is existed
+    # str = "(.*?)A(.*?)p(.*?)l(.*?)n(.*?)s(.*?)"
+    # 'Applications'[Regexp.new str]
+    def similar_match str
+      if !str || str == ''
+        return Regexp.new('.*')
+      end
+      str_array = str.split('')
+      reg_str = '(.*?)'
+      str_array.each do |x|  
+        reg_str += "#{x}(.*?)"
+      end
+
+      return reg_str
+    end
+
     # 根据 input 内容过滤 json 中的 key
     def get_matched_keys(file_name_mapping, filter_key)
-      # filter_key = filter_key.downcase
       matchedKeys = []
-      if file_name_mapping[filter_key]
-        matchedKeys << filter_key
-      else
-        # 相似匹配
-        # TODO 更智能
-        file_name_mapping.keys.each do |file_name_mappingKey|
-          if file_name_mappingKey.include? filter_key
-            matchedKeys << file_name_mappingKey
+        reg_str = similar_match filter_key.downcase
+        file_name_mapping.keys.each do |file_name_mapping_key|
+        if file_name_mapping_key.downcase[Regexp.new reg_str]
+          if file_name_mapping[filter_key] && filter_key == file_name_mapping_key
+            matchedKeys.unshift(file_name_mapping_key)
+          else
+            matchedKeys << file_name_mapping_key
           end
         end
       end
